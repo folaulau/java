f58eeae1999f3be6783ff9f195efa32d620d2232
Folau <folaukaveinga@gmail.com>
2018-07-25 15:18:22 -0700

    added regex

---
+++ java-regex/src/main/java/com/kaveinga/javaregex/JavaRegexApplication.java
@@ -0,0 +1,12 @@
+package com.kaveinga.javaregex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRegexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRegexApplication.class, args);
+	}
+}
