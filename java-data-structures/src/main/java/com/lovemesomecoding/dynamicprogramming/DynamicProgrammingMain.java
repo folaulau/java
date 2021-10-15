3d5718a101269f77c2460f6de78ddac35fd4b3ff
Folau Kaveinga <folaukaveinga@gmail.com>
2021-10-15 11:31:07 -0600

    added dynamic programming

---
+++ java-data-structures/src/main/java/com/lovemesomecoding/dynamicprogramming/DynamicProgrammingMain.java
@@ -0,0 +1,81 @@
+package com.lovemesomecoding.dynamicprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynamicProgrammingMain {
+
+    /**
+     * Recursion can be done with for loop<br>
+     * 
+     * 1. base case(return value)<br>
+     * 2. recursive case(call itself with reduced parameter value)
+     */
+
+    public static void main(String[] args) {
+        int fibonacciOf = fibonacciWithCache(new HashMap<>(), 9);
+        System.out.println("fibonacciWithCache of 9: " + fibonacciOf);
+
+        fibonacciOf = fibonacciWithTabulation(9);
+        System.out.println("fibonacciWithTabulation of 9: " + fibonacciOf);
+    }
+
+    /**
+     * Dynamic Programming with Tabulation<br>
+     */
+    private static int fibonacciWithTabulation(int num) {
+
+        if (num <= 0) {
+            return 0;
+        }
+
+        int storage[] = new int[num + 1];
+
+        /**
+         * first and second case are pre-computed
+         */
+        /*
+         * first case
+         */
+        storage[0] = 0;
+        /*
+         * second case
+         */
+        storage[1] = 1;
+
+        for (int i = 2; i <= num; i++) {
+
+            storage[i] = storage[i - 1] + storage[i - 2];
+
+            System.out.println(storage[i - 1] + "+" + storage[i - 2] + " = " + storage[i]);
+
+        }
+
+        return storage[num];
+    }
+
+    /**
+     * Dynamic Programming with Memoization<br>
+     * storage can be a map, array, or list depending on your situation.
+     */
+    private static int fibonacciWithCache(Map<Integer, Integer> storage, int num) {
+        if (num <= 1) {
+            return num;
+        }
+
+        if (storage.containsKey(num)) {
+            return storage.get(num);
+        }
+
+        int number1 = fibonacciWithCache(storage, num - 1);
+        int number2 = fibonacciWithCache(storage, num - 2);
+
+        int result = number1 + number2;
+
+        System.out.println(number1 + "+" + number2 + " = " + result);
+
+        storage.put(num, result);
+
+        return result;
+    }
+}
--- java-data-structures/src/main/java/com/lovemesomecoding/recursion/RecursionMain.java
+++ java-data-structures/src/main/java/com/lovemesomecoding/recursion/RecursionMain.java
@@ -23,12 +23,6 @@ public class RecursionMain {
         int fibonacciOf = fibonacci(9);
         System.out.println("fibonacci of 9: " + fibonacciOf);
 
-        fibonacciOf = fibonacciWithCache(new HashMap<>(), 9);
-        System.out.println("fibonacciWithCache of 9: " + fibonacciOf);
-
-        fibonacciOf = fibonacciWithTabulation(9);
-        System.out.println("fibonacciWithTabulation of 9: " + fibonacciOf);
-
         fibonacciOf = fibonacciWithLoop(9);
         System.out.println("fibonacciWithLoop of 9: " + fibonacciOf);
 
@@ -125,65 +119,6 @@ public class RecursionMain {
         }
 
         return sum;
-    }
-
-    /**
-     * Dynamic Programming with Tabulation<br>
-     */
-    private static int fibonacciWithTabulation(int num) {
-
-        if (num <= 0) {
-            return 0;
-        }
-
-        int storage[] = new int[num + 1];
-
-        /**
-         * first and second case are pre-computed
-         */
-        /*
-         * first case
-         */
-        storage[0] = 0;
-        /*
-         * second case
-         */
-        storage[1] = 1;
-
-        for (int i = 2; i <= num; i++) {
-
-            storage[i] = storage[i - 1] + storage[i - 2];
-
-            System.out.println(storage[i - 1] + "+" + storage[i - 2] + " = " + storage[i]);
-
-        }
-
-        return storage[num];
-    }
-
-    /**
-     * Dynamic Programming with Memoization<br>
-     * storage can be a map, array, or list depending on your situation.
-     */
-    private static int fibonacciWithCache(Map<Integer, Integer> storage, int num) {
-        if (num <= 1) {
-            return num;
-        }
-
-        if (storage.containsKey(num)) {
-            return storage.get(num);
-        }
-
-        int number1 = fibonacciWithCache(storage, num - 1);
-        int number2 = fibonacciWithCache(storage, num - 2);
-
-        int result = number1 + number2;
-        
-        System.out.println(number1 + "+" + number2 + " = " + result);
-
-        storage.put(num, result);
-
-        return result;
     }
 
     private static int sum(int num) {
