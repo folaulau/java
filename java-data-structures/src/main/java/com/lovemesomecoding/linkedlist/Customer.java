41039fd3d247f76e55f35f8c208a665f171cd639
Folau Kaveinga <folaukaveinga@gmail.com>
2021-11-01 11:42:33 -0600

    added java priority queue

--- java-data-structures/src/main/java/com/lovemesomecoding/linkedlist/Customer.java
+++ java-data-structures/src/main/java/com/lovemesomecoding/linkedlist/Customer.java
@@ -1,5 +1,7 @@
 package com.lovemesomecoding.linkedlist;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Customer {
+public class Customer implements Serializable, Comparable<Customer> {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
     private String firstName;
     private String lastName;
     private String email;
+    
+    @Override
+    public int compareTo(Customer other) {
+        // TODO Auto-generated method stub
+        return this.email.compareTo(other.getEmail());
+    }
 }
--- java-data-structures/src/main/java/com/lovemesomecoding/queue/QueueMain.java
+++ java-data-structures/src/main/java/com/lovemesomecoding/queue/QueueMain.java
@@ -1,24 +1,38 @@
 package com.lovemesomecoding.queue;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 import com.lovemesomecoding.linkedlist.Customer;
 
 public class QueueMain {
 
     public static void main(String[] args) {
         MyQueue<Customer> queue = new MyQueue<>();
-        
+
         System.out.println("Enqueuing 3 customers");
         queue.enqueue(new Customer("Folau", "Kaveinga", "folau@gmail.com"));
         queue.enqueue(new Customer("Lisa", "Kaveinga", "lisa@gmail.com"));
         queue.enqueue(new Customer("Laulau", "Kaveinga", "laulau@gmail.com"));
-        
-        queue.print();
-        
-        System.out.println("Dequeuing...");
-        queue.dequeue();
-        
+
         queue.print();
 
+        System.out.println("Dequeuing...");
+        queue.dequeue();
+
+        queue.print();
+
+        System.out.println("Java PriorityQueue");
+        Queue<Customer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Customer("Folau", "Kaveinga", "folau@gmail.com"));
+        priorityQueue.add(new Customer("Lisa", "Kaveinga", "lisa@gmail.com"));
+        priorityQueue.add(new Customer("Laulau", "Kaveinga", "laulau@gmail.com"));
+
+        priorityQueue.forEach(cust -> {
+            System.out.println(cust.toString());
+        });
+        System.out.println("Polling");
+        System.out.println(priorityQueue.poll().toString()); 
     }
 
 }
